7c0ba4d069fe31d24b4eb69b9df0d8a1b1eea624
ibrahim maher mohamed <61459214+ibrahim-maher@users.noreply.github.com>
2024-12-01 16:18:52 +0200

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
